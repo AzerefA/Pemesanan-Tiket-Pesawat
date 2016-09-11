ccab4cc0c7fe2ab897390f0a0dadd35f875c4b1c
M.In'am Awaludin <darkdm003@gmail.com>
2016-09-11 15:37:15 +0700

    1.Tahap Pendesignan layout dan panambahan component 2.Pendeklarasian Variable component

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1020/pemesanantiketpesawat/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1020/pemesanantiketpesawat/MainActivity.java
@@ -2,12 +2,37 @@ package id.sch.smktelkom_mlg.tugas01.xirpl1020.pemesanantiketpesawat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNama,etKtp;
+    Button bPesan;
+    Spinner kotaA,kotaT;
+    RadioGroup rgMas;
+    CheckBox cbMak,cbSel,cbTax;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etKtp = (EditText) findViewById(R.id.editTextKtp);
+        bPesan = (Button) findViewById(R.id.buttonPesan);
+        rgMas = (RadioGroup) findViewById(R.id.radioGroupMas);
+        kotaA = (Spinner)findViewById(R.id.spinnerAsal);
+        kotaT = (Spinner) findViewById(R.id.spinnerTujuan);
+        cbMak = (CheckBox) findViewById(R.id.checkBoxMakan);
+        cbSel = (CheckBox) findViewById(R.id.checkBoxSelimut);
+        cbTax = (CheckBox) findViewById(R.id.checkBoxTaxi);
+
+
+
     }
 }
